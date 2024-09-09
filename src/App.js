1737456b69f6290e36e6b26e949cc3d6cdbb7893
roryn959 <zcabrbn@ucl.ac.uk>
2024-09-09 19:17:16 +0100

    added toast to explain server spinup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import clock from './assets/clock.jpeg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Toast, ToastContainer } from 'react-bootstrap';
+import { useState } from 'react';
 import useToken from './hooks/useToken';
 import useUsername from './hooks/useUsername';
 import Main from './Main';
@@ -18,6 +21,7 @@ import About from './components/About';
 function App() {
   const [token, setToken] = useToken();
   const usernameState = useUsername(token);
+  const [showToast, setShowToast] = useState(true);
 
   if (!token){
     return (
@@ -31,7 +35,28 @@ function App() {
           <Route path='/about' element={<About/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
+
         <Footer/>
+
+        <ToastContainer position='top-end' className='m-3'>
+          <Toast className='ms-3' show={showToast} onClose={() => setShowToast(false)}>
+            <Toast.Header className='me-2'>
+              <img src={clock} height={25} alt="roryn959@icloud.com" className="m-2"/>
+              <strong className="me-auto text-primary">Server Spinup</strong>
+            </Toast.Header>
+            <Toast.Body>
+              <p>
+                The backend server of this app sits idle whilst unused to conserve energy. When used, it takes a few seconds
+                to 'spin up'.
+              </p>
+              <p>
+                This means when first used, this app may have a small delay when a certain feature is used. Please be
+                patient, as this delay will disappear once the app has warmed up!
+              </p>
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+
       </div>
     </Router>
     );
@@ -49,7 +74,28 @@ function App() {
           <Route path='/about' element={<About/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
+
         <Footer/>
+
+        <ToastContainer position='top-end' className='m-3'>
+          <Toast className='ms-3' show={showToast} onClose={() => setShowToast(false)}>
+            <Toast.Header className='me-2'>
+              <img src={clock} height={25} alt="roryn959@icloud.com" className="m-2"/>
+              <strong className="me-auto text-primary">Server Spinup</strong>
+            </Toast.Header>
+            <Toast.Body>
+              <p>
+                The backend server of this app sits idle whilst unused to conserve energy. When used, it takes a few seconds
+                to 'spin up'.
+              </p>
+              <p>
+                This means when first used, this app may have a small delay when a certain feature is used. Please be
+                patient, as this delay will disappear once the app has warmed up!
+              </p>
+            </Toast.Body>
+          </Toast>
+        </ToastContainer>
+
       </div>
     </Router>
   );
